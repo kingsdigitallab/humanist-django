e9a787c3583656c00efe6030a62e4e4f6962a6f3
Brian Maher <brian.maher@kcl.ac.uk>
2018-08-14 15:05:53 +0000

    Admin

--- humanist_app/admin.py
+++ humanist_app/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin # noqa
+from django.contrib import admin  # noqa
 
 # Register your models here.
+from .models import IncomingEmail, EditedEmail, Edition, Subscriber
+
+
+@admin.register(IncomingEmail)
+class IncomingEmailAdmin(admin.ModelAdmin):
+    list_display = ['date', 'sender', 'subject', 'used']
+
+
+@admin.register(EditedEmail)
+class EditedEmailAdmin(admin.ModelAdmin):
+    list_display = ['edition', 'sender', 'subject']
+
+
+@admin.register(Edition)
+class EditionAdmin(admin.ModelAdmin):
+    list_display = ['date_created', 'subject', 'sent', 'date_sent']
+
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ['user', 'bio']
